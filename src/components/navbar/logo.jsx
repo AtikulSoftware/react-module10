c9888b595aa081d3f8f90a9a097ba35ecc9e898a
AtikulSoftware <atikul.official2020@gmail.com>
2025-03-20 19:17:06 +0600

    Added responsive navbar component & lucide icons package

---
+++ src/components/navbar/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <>
+            <div>
+                <img src="/logo.png" alt="wizia" className="h-6" />
+            </div>
+        </>
+    )
+}
+
+export default Logo;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,33 @@
+import { Menu } from 'lucide-react';
 import React from 'react';
+import Container from '../container';
+import Logo from './logo';
 
 const Navbar = () => {
     return (
         <>
-            <h1>navbar</h1>
+            <nav className="bg-[var(--primary-bg-color)]">
+                <Container>
+                    <div className='flex justify-between items-center py-4 text-white'>
+                        <Logo />
+                        <ul className="hidden sm:flex space-x-5 font-WorkSans">
+                            <li><a href="/">About Us</a></li>
+                            <li><a href="/about">Pricing</a></li>
+                            <li><a href="/contact">Customers</a></li>
+                            <li><a href="/contact">Solutions</a></li>
+                        </ul>
+                        <div className="space-x-5 hidden md:flex">
+                            <button className="bg-[var(--secondary-bg-color)] px-4 py-2 rounded-full text-black font-semibold">Book a Demo</button>
+                            <button className="border-2 border-white rounded-full px-3 py-2">Contact Us</button>
+                        </div>
+                        <div className="block sm:hidden">
+                            <button>
+                                <Menu />
+                            </button>
+                        </div>
+                    </div>
+                </Container>
+            </nav>
         </>
     )
 }
