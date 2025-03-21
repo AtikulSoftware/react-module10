21ea2e2c63e8d9aedffeb5ca22e0fc55b8be01e3
AtikulSoftware <atikul.official2020@gmail.com>
2025-03-21 17:18:11 +0600

    footer section responsive

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <>
             <footer className="py-6 border-t-[1px] border-[var(--border-color)] bg-[#07292F]">
                 <Container>
-                    <div className="flex items-center justify-between">
+                    <div className="flex gap-4 items-center justify-between">
                         <img src="/images/footer-logo.png" alt="wizia" className="h-5" />
                         <p className="text-[#96ACAF] text-sm">Copyright © 2023 Wizia. All rights reserved.</p>
                     </div>
