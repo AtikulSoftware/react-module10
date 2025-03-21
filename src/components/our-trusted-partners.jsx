70edc3b3e4cd17f10fa9470ae0339ecc05c79d61
AtikulSoftware <atikul.official2020@gmail.com>
2025-03-21 17:25:57 +0600

    OurTrustedPartners Section Responsive

--- src/components/our-trusted-partners.jsx
+++ src/components/our-trusted-partners.jsx
@@ -7,7 +7,7 @@ const OurTrustedPartners = () => {
                 <Container>
                     <div className="flex flex-col gap-2 items-center justify-center">
                         <h1 className="uppercase text-[var(--secondary-bg-color)]">our trusted partners</h1>
-                        <div className="flex flex-wrap items-center justify-center gap-1">
+                        <div className="flex flex-wrap items-center justify-center gap-4">
                             <img src="/images/back-hub-logo.png" alt="back hub logo" className="h-14" />
                             <img src="/images/cable-labs-logo.png" alt="cable labs logo.png" className="h-14" />
                             <img src="/images/dbs-logo.png" alt="dbs logo" className="h-14" />
