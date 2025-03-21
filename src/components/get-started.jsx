c8247c9f1265a6b3d89602a5d044d18afefd787b
AtikulSoftware <atikul.official2020@gmail.com>
2025-03-21 17:31:25 +0600

    Adjust Fonts in GetStarted

--- src/components/get-started.jsx
+++ src/components/get-started.jsx
@@ -9,7 +9,7 @@ const GetStarted = () => {
                 <div className="flex items-center justify-center text-center gap-4 text-white py-10">
                     <div className="flex flex-col gap-6 items-center justify-center max-w-3xl">
                         <h1 className="uppercase text-[var(--secondary-bg-color)] ">get started</h1>
-                        <h1 className="italic text-2xl">Embrace <span className="font-semibold">the new era of</span> outbound.</h1>
+                        <h1 className="italic text-2xl">Embrace <span className="font-semibold not-italic">the new era of</span> outbound.</h1>
                         <p>Wizia lets you train, activate, and optimize aiDRs.
                             Take your outbound to new levels of performance and efficiency.</p>
                         <SignUp />
